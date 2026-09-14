972974e2a501a1decd6fa56874e55775de8efe27
samuelcortezsux-creator <samuelcortezsux@gmail.com>
2026-09-13 22:54:03 -0400

    mlo

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,6 @@ import Footer from './components/Footer'
 import Home from './pages/Home'
 import Carrito from './pages/Carrito'
 
-// App.jsx es el componente "padre":
-// - CarritoProvider da acceso al carrito a toda la app.
-// - BrowserRouter + Routes deciden qué página se muestra según la URL:
-//   "/"        -> Home (todas las secciones de siempre)
-//   "/carrito" -> Carrito (la página nueva)
-// Navbar y Footer quedan FUERA de <Routes> porque se ven en ambas páginas.
 function App() {
   return (
     <CarritoProvider>
