a4924a7c901e63a1e48354b5eb0d9cdf46dabd02
samuelcortezsux-creator <samuelcortezsux@gmail.com>
2026-09-13 22:47:41 -0400

    pmj

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
 
-
+// Aquí React "engancha" toda la aplicación (App) dentro
+// del <div id="root"></div> que está en index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
